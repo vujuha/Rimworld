0053fda23ea8c898ac2565f9e4e24c0c70f94dd6
vujuha <79193526+vujuha@users.noreply.github.com>
2022-12-01 03:22:28 +0300

    SandPump

---
+++ SandPump/Source/SandPump/SandPump/CompTerrainPumpSand.cs
@@ -0,0 +1,58 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.Sound;
+using Verse;
+
+namespace SandPump
+{
+    public class CompTerrainPumpSand : CompTerrainPump
+    {
+        private Sustainer sustainer;
+
+        private CompProperties_TerrainPumpSand Props => (CompProperties_TerrainPumpSand)props;
+
+        protected override void AffectCell(IntVec3 c)
+        {
+            AffectCell(parent.Map, c);
+        }
+
+        public static void AffectCell(Map map, IntVec3 c)
+        {
+            TerrainDef terrain = c.GetTerrain(map);
+            if (terrain == TerrainDefOf.Sand)
+            {
+                map.terrainGrid.SetTerrain(c, TerrainDefOf.Soil);
+            }
+        }
+
+        public override void PostDeSpawn(Map map)
+        {
+            base.PostDeSpawn(map);
+            if (sustainer != null && !sustainer.Ended)
+            {
+                sustainer.End();
+            }
+        }
+
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+            if (!Props.soundWorking.NullOrUndefined() && base.Working && base.CurrentRadius < Props.radius - 0.0001f)
+            {
+                if (sustainer == null || sustainer.Ended)
+                {
+                    sustainer = Props.soundWorking.TrySpawnSustainer(SoundInfo.InMap(parent));
+                }
+                sustainer.Maintain();
+            }
+            else if (sustainer != null && !sustainer.Ended)
+            {
+                sustainer.End();
+            }
+        }
+    }
+}
---
+++ SandPump/Source/SandPump/SandPump/SandPump CompProperties.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SandPump
+{
+    public class CompProperties_TerrainPumpSand : CompProperties_TerrainPump
+    {
+        public SoundDef soundWorking;
+
+        public CompProperties_TerrainPumpSand()
+        {
+            compClass = typeof(CompTerrainPumpSand);
+        }
+    }
+
+}
